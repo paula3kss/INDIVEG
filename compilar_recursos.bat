657c63f901f054fe36e04c5a63dba478f4356df7
Paula de Andrés Anaya <64989905+paula3kss@users.noreply.github.com>
2020-05-08 09:00:43 +0200

    Add files via upload

---
+++ compilar_recursos.bat
@@ -0,0 +1,5 @@
+call "C:\Program Files\QGIS 3.10\bin\o4w_env.bat"
+call "C:\Program Files\QGIS 3.10\bin\qt5_env.bat"
+call "C:\Program Files\QGIS 3.10\bin\py3_env.bat"
+set PATH=%PATH%;C:\Program Files\QGIS 3.10\apps\Python37\Scripts\
+pyrcc5 -o resources.py resources.qrc
